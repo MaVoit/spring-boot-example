45a6250fd5a96d61a1dbfa83a8dbb7c238d0343d
MaVoit <ma.voit@gmail.com>
2015-05-07 13:26:10 +0300

    code cleanup

--- src/main/java/com/voit/example/Application.java
+++ src/main/java/com/voit/example/Application.java
@@ -2,7 +2,6 @@ package com.voit.example;
 
 import com.voit.example.config.AppConfig;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
 
 /**
  * @author mark voit
@@ -10,6 +9,6 @@ import org.springframework.context.ApplicationContext;
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(AppConfig.class, args);
+        SpringApplication.run(AppConfig.class, args);
     }
 }
